76a47d5ee766e3ee01d6d3f127d52b13d888cfff
Matías Maximiliano Simone <matias.maximiliano.simone@gmail.com>
2019-05-04 19:04:01 -0300

    added tests

--- src/test/SortTest.java
+++ src/test/SortTest.java
@@ -11,53 +11,81 @@ class SortTest {
 
 	@Test
 	void selecctionSortAscTest() {
-		ArrayList<Integer> vec = new ArrayList<Integer>();
-		vec.add(55);
-		vec.add(15);
-		vec.add(45);
-		vec.add(5);
-		vec.add(35);
-		
-		Sort sort = new Sort();
-		sort.selection(vec);
-		
+		ArrayList<Integer> vecRandom = new ArrayList<Integer>();
+		ArrayList<Integer> vecSorted = new ArrayList<Integer>();
+		ArrayList<Integer> vecReverse = new ArrayList<Integer>();
 		ArrayList<Integer> vecExpected = new ArrayList<Integer>();
+		Sort sort = new Sort();
+		
+		vecRandom.add(55);
+		vecRandom.add(15);
+		vecRandom.add(45);
+		vecRandom.add(5);
+		vecRandom.add(35);
+		sort.selection(vecRandom);
+		
+		vecSorted.add(5);
+		vecSorted.add(15);
+		vecSorted.add(35);
+		vecSorted.add(45);
+		vecSorted.add(55);
+		sort.selection(vecSorted);
+		
+		vecReverse.add(55);
+		vecReverse.add(45);
+		vecReverse.add(35);
+		vecReverse.add(15);
+		vecReverse.add(5);
+		sort.selection(vecReverse);
+		
 		vecExpected.add(5);
 		vecExpected.add(15);
 		vecExpected.add(35);
 		vecExpected.add(45);
 		vecExpected.add(55);
-		System.out.println("SelecctionSort");
-		for(Integer v:vecExpected) {
-			System.out.println(v);
-		}
 		
-		Assert.assertArrayEquals(vecExpected.toArray(), vec.toArray());
+		Assert.assertArrayEquals(vecExpected.toArray(), vecRandom.toArray());
+		Assert.assertArrayEquals(vecExpected.toArray(), vecSorted.toArray());
+		Assert.assertArrayEquals(vecExpected.toArray(), vecReverse.toArray());
 	}
 	
 	@Test
 	void bubbleSortAscTest() {
-		ArrayList<Integer> vec = new ArrayList<Integer>();
-		vec.add(55);
-		vec.add(15);
-		vec.add(45);
-		vec.add(5);
-		vec.add(35);
-		
-		Sort sort = new Sort();
-		sort.bubble(vec);
-		
+		ArrayList<Integer> vecRandom = new ArrayList<Integer>();
+		ArrayList<Integer> vecSorted = new ArrayList<Integer>();
+		ArrayList<Integer> vecReverse = new ArrayList<Integer>();
 		ArrayList<Integer> vecExpected = new ArrayList<Integer>();
+		Sort sort = new Sort();
+		
+		vecRandom.add(55);
+		vecRandom.add(15);
+		vecRandom.add(45);
+		vecRandom.add(5);
+		vecRandom.add(35);
+		sort.bubble(vecRandom);
+		
+		vecSorted.add(5);
+		vecSorted.add(15);
+		vecSorted.add(35);
+		vecSorted.add(45);
+		vecSorted.add(55);
+		sort.bubble(vecSorted);
+		
+		vecReverse.add(55);
+		vecReverse.add(45);
+		vecReverse.add(35);
+		vecReverse.add(15);
+		vecReverse.add(5);
+		sort.bubble(vecReverse);
+		
 		vecExpected.add(5);
 		vecExpected.add(15);
 		vecExpected.add(35);
 		vecExpected.add(45);
 		vecExpected.add(55);
-		System.out.println("BubbleSort");
-		for(Integer v:vecExpected) {
-			System.out.println(v);
-		}
 		
-		Assert.assertArrayEquals(vecExpected.toArray(), vec.toArray());
+		Assert.assertArrayEquals(vecExpected.toArray(), vecRandom.toArray());
+		Assert.assertArrayEquals(vecExpected.toArray(), vecSorted.toArray());
+		Assert.assertArrayEquals(vecExpected.toArray(), vecReverse.toArray());
 	}
 }
